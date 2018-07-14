3faa3ab9aeada88c21cc272a0a8ed8565e5f7a37
Maxim Tsvetkov <777.maxxx@gmail.com>
2018-07-14 19:11:33 +0200

    app factory updated to configure grid view controller

--- DemoGifBrowser/Coordinator/AppFactory.swift
+++ DemoGifBrowser/Coordinator/AppFactory.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2018 MAXIM TSVETKOV. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AppFactory {
+    
+    private let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    
+    func gridViewController() -> GridViewController {
+        
+        let giphyService = GiphyServiceImp()
+        let viewModel = GridViewModelImp(giphyService: giphyService)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "GridViewController") as! GridViewController
+        
+        viewController.viewModel = viewModel
+        
+        return viewController
+    }
+}
